eea5ad4f3b2367d5c4301f6ddb7f3e2b53ea244c
Suraj Gurung <sgrg007@gmail.com>
2020-05-23 01:29:30 -0500

    add display taal

---
+++ src/main/java/com/sdet/auto/springbootjava/suraj/surajPhewaTaal.java
@@ -0,0 +1,12 @@
+package com.sdet.auto.springbootjava.suraj;
+
+public class surajPhewaTaal {
+    public static void main(String[] args) {
+        surajPhewaTaal suj1=new surajPhewaTaal();
+        suj1.displayphewaTaal();
+
+    }
+    public void displayphewaTaal(){
+        System.out.println("print PhewaTaal");
+    }
+}
